065c7d31409203caa484918a404d3c7cd2d61190
kazumits <kazumits@chromatin.med.kyushu-u.ac.jp>
2011-05-15 01:28:02 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McPipeline::Application.config.secret_token = '5e510b77363b6ef5149eb5b73ded2df5411b9b0ab343dda693321161538abd4fd44d88983f23284ce370c985a7afe4b6616a1ad7b970a74dfe67bd069342cc22'
