6203368e704a34e5cd4d7924e6a9babc0932f286
Kazumitsu MAEHARA <kazumits@gmail.com>
2011-05-15 18:34:41 +0900

    complete getClusterInfo/getQueueInfo

--- app/helpers/gadgets_helper.rb
+++ app/helpers/gadgets_helper.rb
@@ -1,5 +1,6 @@
 module GadgetsHelper
   require 'net/ssh'
+  require 'rexml/document'
   def hgc_exec(cmd)
     sgeroot = '/home/geadmin/N1GE/'
     sgebin = "#{sgeroot}/bin/lx24-amd64"
@@ -10,5 +11,23 @@ module GadgetsHelper
       result = ssh.exec!(cmd)
     end
     return result
+  end
+
+  def getClusterInfo
+	xml = self.hgc_exec('qstat -g c -xml')
+	doc = REXML::Document.new xml
+	result = doc.elements['/job_info'].elements.map do |x|
+      Hash[x.elements.map {|y| [y.name, y.get_text] }]
+	end
+    return result
+  end
+
+  def getQueueInfo
+	xml = self.hgc_exec('qstat -xml')
+	doc = REXML::Document.new xml
+	result = doc.elements['/job_info'].elements.map do |x|
+      x.elements.map {|y| Hash[y.elements.map {|z| [z.name, z.get_text]}]}
+	end
+    return result.flatten
   end
 end
